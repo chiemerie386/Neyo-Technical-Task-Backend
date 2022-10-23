cebd832dca024bd261645ff7a29f51d9f1bfe209
Chiemerie <chiemerieanaebo@gmail.com>
2022-10-23 15:11:16 +0100

    added authorization middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt =  require("jsonwebtoken")
+const verifyUser = async (req, res, next) => {
+    try {
+      const token = req.headers.authorization?.split(' ')[1] || req.body.token;
+      if (!token) {
+        return res.status(403).json({ status:false, message: 'A token is required for authentication' });
+      }
+  
+      if (token) {
+        const tokenData = jwt.verify(token, process.env.JWT_SECRET);
+        if (new Date().getTime() < tokenData.exp) {
+          return res.status(401).json({ status:false, message: 'Token has expired, reauthenticate again' });
+        }
+  
+        req.userId = tokenData.id;
+        req.email = tokenData.email;
+      }
+      return next();
+    } catch (error) {
+        console.log(error)
+      return res.status(500).send({ status:false, message: "Unable to authorize User " });
+    }
+  };
+
+module.exports = verifyUser
--- src/routes/sketch-routes.js
+++ src/routes/sketch-routes.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const sketchController = require("../controllers/sketch-controller")
+const authorizeUser = require("../middlewares/auth")
 
-/* GET users listing. */
-router.get('/getsketch/:sketchId', sketchController.getSketch);
-router.get('/getSketch', sketchController.getSketch);
+router.use(authorizeUser)
+router.get('/getsketch/:sketchId',  sketchController.getSketch);
+router.get('/getSketch',  sketchController.getSketch);
 router.post('/create', sketchController.createSketch);
 
 
